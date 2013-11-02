2a7c17ff13388a65d4a5d16a9cfbd8a66c7bec65
Fahim Imaduddin Dalvi <fid@qatar.cmu.edu>
2013-11-02 19:53:00 +0300

    Added basic real-time document editing and collaboration

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PhDWriter
+
+This version of PhDWriter requires NodeJS to be installed. 
+
+Once NodeJS is installed, run the following commands to complete the "installation" progress:
+
+# node_modules is ignored in .gitignore, hence to run this app, you will be require to run:
+	npm install
+# This is to avoid cross-compatibility issues across machines and OSes. 
---
+++ app.js
@@ -0,0 +1,46 @@
+
+/**
+ * Module dependencies.
+ */
+
+require('coffee-script');
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , connect = require('./node_modules/connect')
+  , sharejs = require('./node_modules/share');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var options = {db: {type: 'none'}}; // See docs for options. {type: 'redis'} to enable persistance.
+
+// Attach the sharejs REST and Socket.io interfaces to the server
+sharejs.server.attach(app, options);
+
+app.listen(8000, function(){
+    console.log('Server running at http://127.0.0.1:8000/');
+});
